3ad55c1fcfeb2261f124336fdcef47d8a879773e
Raffi Vartanian <rafivar@gmail.com>
2011-11-16 17:35:56 +0200

    fix read_char.downcase, change storage_with_man to storage_with_box, change levels.txt to @levels_filename

--- sokoban.rb
+++ sokoban.rb
@@ -1,151 +1,180 @@
-$board = []
-$x = -1
-$y = -1
-
-def cls
-  system('cls')
-end
-
-def read_char
-        require "Win32API"
-        Win32API.new("crtdll", "_getch", [], "L").Call
-end
-
-def print_board
-	$board.each do |line|
-		line.each do |char|
-			print char
-		end
-	end
-end
-
-#This function is not being used in the program, but it's good to have
-def in_board?(x, y)
-	if (x >= 0 && y >=0 && x < $board.size && y < $board[x].size - 1)
-		true
-	else
-		false
-	end
-end
-
-def check_for_win
-	$board.each do |line|
-		line.each do |char|
-			if char == 'o'
-				return false
-			end
-		end
-	end
-	puts "WIN :)"
-	return true
-end
-
-def move_box(x, y)					
-	case $board[x][y]
-		when " "	#move box to free space
-			$board[x][y] = "o"								
-		when "."	#move box to storage spot
-			$board[x][y] = "*"	
-	end
-end
-
-def move_man (x, y)
-	#draw man at new position
-	if $board[x][y] == " " || $board[x][y] == "o"
-		$board[x][y] = "@"
-	elsif $board[x][y] == "." || $board[x][y] == "*"
-		$board[x][y] = "+"
-	end
-	
-	#clear man from previous position
-	if $board[$x][$y] == "@"
-		$board[$x][$y] = " "
-	else
-		$board[$x][$y] = "."
-	end
-	
-	#chage man to new position
-	$x = x
-	$y = y
-end
-
-def play(command)
-	valid_command = true
-	
-	case command 
-		when 119 #up (W)
-			x1 = $x-1
-			y1 = $y
-			x2 = $x-2
-			y2 = $y
-		when 115 #down (S)
-			x1 = $x+1
-			y1 = $y
-			x2 = $x+2
-			y2 = $y
-		when 97	 #left (A)
-			x1 = $x
-			y1 = $y-1
-			x2 = $x
-			y2 = $y-2
-		when 100 #right (D)
-			x1 = $x
-			y1 = $y+1
-			x2 = $x
-			y2 = $y+2
-		else
-			valid_command = false
-	end
-	
-	if valid_command	#if command is valid
-		if $board[x1][y1] != "#"	#if the spot near isn't a wall
-			case $board[x1][y1]
-				when " ", "."	#if the spot near the man is free, go there
-					move_man(x1, y1)
-				when "o", "*"	#if the spot near the man is not free, check the next spot
-					case $board[x2][y2]
-						when " ", "."	#if the next spot is free
-							move_box(x2, y2)
-							move_man(x1, y1)
-					end
-			end
-		end
-		#print board
-		cls
-		print_board
-	end
-end
-
-######## SAVE LEVEL STATE INTO "$board" ########
-open("levels.txt") do |file|
-	line = []
-	while char = file.getc
-		if char != "\n"
-			if char == "@"
-				$x = $board.size
-				$y = line.size
-			end
-			line << char
-		else
-			line << char
-			$board << line
-			line = []
-		end
-	end
-	#end of file is not "\n", but still need to insert the line into the board
-	line << "\n"
-	$board << line
-end
-
-######### START THE GAME ##########
-cls
-print_board
-
 begin
-	command = read_char
-	play(command)
-end while command != 113 && check_for_win == false	#quit (Q)
+  require 'Win32API'
 
-if command == 113
-	puts "Thanks for playing. come back soon! :)"
+  def cls
+    system 'cls'
+  end
+
+  def read_char
+    require "Win32API"
+    Win32API.new("crtdll", "_getch", [], "L").Call
+  end
+rescue LoadError
+  def cls
+    system 'clear'
+  end
+
+  def read_char
+    begin
+      system "stty raw -echo"
+      str = STDIN.getc
+    ensure
+      system "stty -raw echo"
+    end
+    str.chr
+  end
 end
+
+class Sokoban
+  def initialize(levels_filename)
+    @levels_filename = levels_filename
+    @board = []
+    @x = -1
+    @y = -1
+  end
+
+  def play
+    read_level
+    find_man
+    cls
+    print_board
+
+    catch :quit do
+      loop do
+        move (read_char.chr.downcase)
+      end
+    end
+
+    puts "Thanks for playing. Come back soon! :)"
+  end
+
+  private
+
+  def man
+    '@'
+  end
+
+  def box
+    'o'
+  end
+
+  def wall
+    '#'
+  end
+
+  def storage
+    '.'
+  end
+
+  def storage_with_man
+    '+'
+  end
+
+  def storage_with_box
+    '*'
+  end
+
+  def space
+    ' '
+  end
+
+  def read_level
+    open @levels_filename do |file|
+      file.each do |line|
+        @board << line.chars.to_a
+      end
+    end
+  end
+
+  def find_man
+    @board.each_with_index do |line, index|
+      if line.include?(man)
+        @x = index
+        @y = line.index man
+      end
+    end
+  end
+
+  def print_board
+    puts @board.map(&:join)
+  end
+
+  # This method is not being used in the program, but it's good to have
+  def in_board?(x, y)
+    x >= 0 && y >= 0 && x < @board.size && y < @board[x].size - 1
+  end
+
+  def won?
+    @board.none? do |line|
+      line.any? { |p| p == box }
+    end
+  end
+
+  def move_box(x, y)
+    @board[x][y] = case object_at x, y
+    when space
+      box
+    when storage
+      storage_with_box
+    end
+  end
+
+  def move_man(x, y)
+    # draw man at new position
+    @board[x][y] = case object_at x, y
+    when space, box
+      man
+    when storage, storage_with_box
+      storage_with_man
+    end
+
+    # clear man from previous position
+    @board[@x][@y] = object_at(@x, @y) == man ? space : storage
+
+    # chage man to new position
+    @x, @y = x, y
+  end
+
+  def object_at(x, y)
+    @board[x][y]
+  end
+
+  def move(command)
+    x1, y1, x2, y2 = case command
+    when 'w'
+      [@x - 1, @y, @x - 2, @y]
+    when 's'
+      [@x + 1, @y, @x + 2, @y]
+    when 'a'
+      [@x, @y - 1, @x, @y - 2]
+    when 'd'
+      [@x, @y + 1, @x, @y + 2]
+    when 'q'
+      throw :quit
+    else
+      puts "Invalid command" && return
+    end
+
+    case object_at x1, y1
+    when wall
+    when space, storage
+      move_man x1, y1
+    when box, storage_with_box
+      case object_at x2, y2
+      when space, storage
+        move_box x2, y2
+        move_man x1, y1
+      end
+    end
+
+    cls
+    print_board
+
+    if won?
+      puts "WIN :)"
+      throw :quit
+    end
+  end
+end
+
+Sokoban.new("levels.txt").play
